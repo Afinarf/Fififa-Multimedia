56896e55b693155011fa5a0872ea91dbaad2b5f2
Afinarf <afinafin024@gmail.com>
2025-06-02 13:14:22 +0700

    update responsive

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+            AOS.init();
+
             // Smooth scrolling for navigation links
             document.querySelectorAll('a[href^="#"]').forEach(anchor => {
                 anchor.addEventListener('click', function (e) {
